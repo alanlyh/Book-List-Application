250d1177839367d6d4f629dd005e61ca0f604407
Yuanhao Lu <yl3786@columbia.edu>
2018-12-20 17:30:47 -0500

    feat: complete CRUD for category and book

--- project.py
+++ project.py
@@ -45,7 +45,7 @@ def categories():
 @app.route('/category/<int:category_id>/book')
 def categoryBooks(category_id):
 	books = session.query(Book).filter_by(category_id=category_id).all()
-	return render_template('books.html', books=books)
+	return render_template('books.html', books=books, category_id=category_id)
 
 
 @app.route('/category/<int:category_id>/book/<int:book_id>')
@@ -78,6 +78,72 @@ def deleteCategory(category_id):
         return redirect(url_for('categories'))
     else:
         return render_template("deleteCategory.html", category=category)
+
+
+@app.route('/category/<int:category_id>/edit', methods=['GET', 'POST'])
+def editCategory(category_id):
+    category = session.query(Category).filter_by(id=category_id).one()
+    if request.method == 'POST':
+        category.name = request.form['name']
+        if 'desc' in request.form and request.form['desc'] != "":
+            category.desc = request.form['desc']
+        else:
+            category.desc = None
+        session.add(category)
+        session.commit()
+        return redirect(url_for('categories'))
+    else:
+        return render_template("editCategory.html", category=category)
+
+
+@app.route('/category/<int:category_id>/book/new', methods=['GET', 'POST'])
+def newBook(category_id):
+    if request.method == 'POST':
+        category = session.query(Category).filter_by(id=category_id).one()
+        book = None
+        book = Book(name=request.form['name'], category=category)
+        if 'desc' in request.form and request.form['desc'] != "":
+            book.desc = request.form['desc']
+        if 'author' in request.form and request.form['author'] != "":
+            book.author = request.form['author']
+        session.add(book)
+        session.commit()
+        return redirect(url_for('categoryBooks', category_id=category.id))
+    else:
+        return render_template("newBook.html")
+
+
+@app.route('/category/<int:category_id>/book/<int:book_id>/delete', methods=['GET', 'POST'])
+def deleteBook(category_id, book_id):
+    book = session.query(Book).filter_by(id=book_id).one()
+    if request.method == 'POST':
+        session.delete(book)
+        session.commit()
+        return redirect(url_for('categoryBooks', category_id=category_id))
+    else:
+        return render_template("deleteBook.html", book=book, category_id=category_id)
+
+
+@app.route('/category/<int:category_id>/book/<int:book_id>/edit', methods=['GET', 'POST'])
+def editBook(category_id, book_id):
+    book = session.query(Book).filter_by(id=book_id).one()
+    if request.method == 'POST':
+        book.name = request.form['name']
+        if 'desc' in request.form and request.form['desc'] != "":
+            book.desc = request.form['desc']
+        else:
+            book.desc = None
+        if 'author' in request.form and request.form['author'] != "":
+            book.author = request.form['author']
+        else:
+            book.author = None
+        session.add(book)
+        session.commit()
+        return redirect(url_for('categoryBooks', category_id=category_id))
+    else:
+        return render_template("editBook.html", book=book, category_id=category_id)
+
+
 
 if __name__ == '__main__':
   app.debug = True
